d59a7b4f09cb665582b5e6fba01438aef3fd7bf9
Jan Schaffranek <schaffranek.jan@web.de>
2025-08-11 12:49:46 +0200

    changed api base

--- discord_app.py
+++ discord_app.py
@@ -5,11 +5,12 @@ import aiohttp
 import discord
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-API_BASE = "http://127.0.0.1:8000/api/"
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+port = int(os.environ.get("PORT", 8000))
+api_base = f"http://0.0.0.0:{port}/api/"
 
 
 def create_price_embed(
@@ -61,19 +62,19 @@ async def on_message(
 
     if message.content.startswith("!gear_salvage"):
         title = "Gear Salvage Prices"
-        api_url = API_BASE + "gear_salvage"
+        api_url = api_base + "gear_salvage"
     elif message.content.startswith("!relic_of_fireworks"):
         title = "Relic of Fireworks"
-        api_url = API_BASE + "relic_of_fireworks"
+        api_url = api_base + "relic_of_fireworks"
     elif message.content.startswith("!scholar_rune"):
         title = "Scholar Rune"
-        api_url = API_BASE + "scholar_rune"
+        api_url = api_base + "scholar_rune"
     elif message.content.startswith("!rare_gear_craft"):
         title = "Rare Gear Craft"
-        api_url = API_BASE + "rare_gear_to_ecto"
+        api_url = api_base + "rare_gear_to_ecto"
     elif message.content.startswith("!gear_to_ecto"):
         title = "Rare Gear to Ecto"
-        api_url = API_BASE + "gear_to_ecto"
+        api_url = api_base + "gear_to_ecto"
     elif message.content.startswith("!get_price"):
         item_id = (
             message.content.split(maxsplit=1)[1]
@@ -81,7 +82,7 @@ async def on_message(
             else ""
         )
         title = f"Price for Item ID: {item_id}"
-        api_url = API_BASE + f"price?item_id={item_id}"
+        api_url = api_base + f"price?item_id={item_id}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url) as resp:
