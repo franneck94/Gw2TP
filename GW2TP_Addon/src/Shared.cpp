a6dd3ff6e3b5db10c38e2a2beea5d1842faa86fd
Jan Schaffranek <schaffranek.jan@web.de>
2025-09-03 17:13:08 +0200

    format

--- GW2TP_Addon/src/Shared.cpp
+++ GW2TP_Addon/src/Shared.cpp
@@ -4,5 +4,5 @@ AddonAPI *APIDefs = nullptr;
 NexusLinkData *NexusLink = nullptr;
 RTAPI::RealTimeData *RTAPIData = nullptr;
 
-const char* KB_TOGGLE_GW2TP = "KB_TOGGLE_GW2TP";
- const char* ADDON_NAME = "GW2TP";
+const char *KB_TOGGLE_GW2TP = "KB_TOGGLE_GW2TP";
+const char *ADDON_NAME = "GW2TP";
