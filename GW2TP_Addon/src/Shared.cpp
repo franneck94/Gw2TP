98b3b7f0ddcefc11418006aed15eac9f67c0886c
Jan Schaffranek <schaffranek.jan@web.de>
2025-09-10 09:23:10 +0200

    dropped c++ code here

--- GW2TP_Addon/src/Shared.cpp
+++
@@ -1,8 +0,0 @@
-#include "Shared.h"
-
-AddonAPI *APIDefs = nullptr;
-NexusLinkData *NexusLink = nullptr;
-RTAPI::RealTimeData *RTAPIData = nullptr;
-
-const char *KB_TOGGLE_GW2TP = "KB_TOGGLE_GW2TP";
-const char *ADDON_NAME = "GW2TP";
--- GW2TP_Addon/src/Version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define MAJOR 0
-#define MINOR 4
-#define BUILD 0
-#define REVISION 0
-
-#endif // VERSION_H
