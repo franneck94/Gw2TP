02e9f96a45ac8094482c7e5267dbab9e7fdeb681
Jan Schaffranek <schaffranek.jan@web.de>
2025-10-13 08:22:35 +0200

    Add API endpoints for leather strap data

--- backend/api.py
+++ backend/api.py
@@ -970,6 +970,107 @@ def get_thesis_on_masterful_malice() -> JSONResponse:
     return JSONResponse(content=jsonable_encoder(data))
 
 
+@fastapi_app.get("/thick_leather_strap")
+def get_thick_leather_strap() -> JSONResponse:
+    try:
+        fetched_data = fetch_tp_prices(
+            [
+                ItemIDs.THICK_LEATHER_STRAP,
+                ItemIDs.COARSE_LEATHER,
+                ItemIDs.RUGGED_LEATHER,
+            ],
+        )
+    except Exception as e:
+        return JSONResponse(content=jsonable_encoder({"error": str(e)}))
+
+    rugged_leather_strap_buy = fetched_data[ItemIDs.RUGGED_LEATHER_STRAP]["buy"]
+    coarse_leather_section_sell = fetched_data[ItemIDs.COARSE_LEATHER]["sell"]
+    rugged_leather_section_sell = fetched_data[ItemIDs.RUGGED_LEATHER]["sell"]
+
+    lower_rate = 1.28
+    higher_rate = 0.08
+
+    sell = (coarse_leather_section_sell * lower_rate) + (
+        rugged_leather_section_sell * higher_rate
+    )
+    profit_per_salvage = (sell * TAX_RATE) - rugged_leather_strap_buy
+    profit_per_stack = 250.0 * profit_per_salvage
+
+    data = {
+        **get_sub_dct("buy", rugged_leather_strap_buy),
+        **get_sub_dct("profit_per_salvage", profit_per_salvage),
+        **get_sub_dct("profit_per_stack", profit_per_stack),
+    }
+    return JSONResponse(content=jsonable_encoder(data))
+
+
+@fastapi_app.get("/rugged_leather_strap")
+def get_rugged_leather_strap() -> JSONResponse:
+    try:
+        fetched_data = fetch_tp_prices(
+            [
+                ItemIDs.RUGGED_LEATHER_STRAP,
+                ItemIDs.THICK_LEATHER,
+                ItemIDs.RUGGED_LEATHER,
+            ],
+        )
+    except Exception as e:
+        return JSONResponse(content=jsonable_encoder({"error": str(e)}))
+
+    rugged_leather_strap_buy = fetched_data[ItemIDs.RUGGED_LEATHER_STRAP]["buy"]
+    thick_leather_section_sell = fetched_data[ItemIDs.THICK_LEATHER]["sell"]
+    rugged_leather_section_sell = fetched_data[ItemIDs.RUGGED_LEATHER]["sell"]
+
+    lower_rate = 1.28
+    higher_rate = 0.08
+
+    sell = (thick_leather_section_sell * lower_rate) + (
+        rugged_leather_section_sell * higher_rate
+    )
+    profit_per_salvage = (sell * TAX_RATE) - rugged_leather_strap_buy
+    profit_per_stack = 250.0 * profit_per_salvage
+
+    data = {
+        **get_sub_dct("buy", rugged_leather_strap_buy),
+        **get_sub_dct("profit_per_salvage", profit_per_salvage),
+        **get_sub_dct("profit_per_stack", profit_per_stack),
+    }
+    return JSONResponse(content=jsonable_encoder(data))
+
+
+@fastapi_app.get("/hard_leather_strap")
+def get_hard_leather_strap() -> JSONResponse:
+    try:
+        fetched_data = fetch_tp_prices(
+            [
+                ItemIDs.HARD_LEATHER_STRAP,
+                ItemIDs.THICK_LEATHER,
+                ItemIDs.HARDENED_LEATHER,
+            ],
+        )
+    except Exception as e:
+        return JSONResponse(content=jsonable_encoder({"error": str(e)}))
+
+    hard_leather_strap_buy = fetched_data[ItemIDs.HARD_LEATHER_STRAP]["buy"]
+    thick_leather_section_sell = fetched_data[ItemIDs.THICK_LEATHER]["sell"]
+    hardd_leather_section_sell = fetched_data[ItemIDs.HARDENED_LEATHER]["sell"]
+
+    lower_rate = 1.28
+    higher_rate = 0.08
+
+    sell = (thick_leather_section_sell * lower_rate) + (
+        hardd_leather_section_sell * higher_rate
+    )
+    profit = (sell * TAX_RATE) - hard_leather_strap_buy
+
+    data = {
+        **get_sub_dct("buy", hard_leather_strap_buy),
+        **get_sub_dct("salvage", sell),
+        **get_sub_dct("profit", profit),
+    }
+    return JSONResponse(content=jsonable_encoder(data))
+
+
 middleware = [Middleware(CORSMiddleware, allow_origins=["*"])]
 app = Starlette(
     routes=[
